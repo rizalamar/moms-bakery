c0ba88b1aaddfc2462ce82de8d34dd7d7bde9829
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-17 21:34:23 +0700

    feat: data initilizer

---
+++ src/main/java/com/rizalamar/momsbakery/config/DataInitializer.java
@@ -0,0 +1,33 @@
+package com.rizalamar.momsbakery.config;
+
+import com.rizalamar.momsbakery.domain.Account;
+import com.rizalamar.momsbakery.domain.Role;
+import com.rizalamar.momsbakery.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(!accountRepository.existsByUsername("admin")){
+            Account account = Account.builder()
+                    .username("admin")
+                    .password("Admin12345")
+                    .fullName("Admin Bakery")
+                    .email("admin@bakery.com")
+                    .phone("081345855825")
+                    .role(Role.ADMIN)
+                    .active(true)
+                    .build();
+            accountRepository.save(account);
+        }
+    }
+}
