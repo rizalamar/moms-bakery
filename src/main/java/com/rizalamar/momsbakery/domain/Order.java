57068659b59d0196a7bbcbcbd3a3788c75a9448f
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-18 20:02:44 +0700

    update: add builder annotation

--- src/main/java/com/rizalamar/momsbakery/domain/Order.java
+++ src/main/java/com/rizalamar/momsbakery/domain/Order.java
@@ -1,10 +1,7 @@
 package com.rizalamar.momsbakery.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -14,6 +11,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "orders")
 public class Order extends AbstractAuditingEntity {
--- src/main/java/com/rizalamar/momsbakery/domain/OrderItem.java
+++ src/main/java/com/rizalamar/momsbakery/domain/OrderItem.java
@@ -1,10 +1,7 @@
 package com.rizalamar.momsbakery.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -12,6 +9,7 @@ import java.math.BigDecimal;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "order_items")
 public class OrderItem extends AbstractAuditingEntity{
